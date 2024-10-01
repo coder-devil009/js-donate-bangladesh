572103dce82a1ae0b849f25f1f3ceac188ea2235
coder-devil009 <sharifulshohag009@gmail.com>
2024-10-01 06:34:26 +0600

    another2

--- main.js
+++ main.js
@@ -143,9 +143,10 @@ document.getElementById('btn-donation').addEventListener('click', function () {
 
 // ---- Blog button ------
 
-// document.getElementById('btn-blog').addEventListener('click', function(){
-//     window.location.href = './pages/blog.html';
-// })
+document.getElementById('btn-blog').addEventListener('click', function(){
+    window.location.href = './blog.html';
+})
+
 // document.getElementById('btn-home').addEventListener('click', function () {
 
 //     let blogBalance = textById('blog-balance');
